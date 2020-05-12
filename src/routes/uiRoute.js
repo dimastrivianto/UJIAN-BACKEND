1d9439fc00dd54e30c66c59b399863702cad185d
dimtriv <dimastrivianto@gmail.com>
2020-05-12 16:41:32 +0700

    perubahan stock menjadi inventory

--- src/routes/uiRoute.js
+++ src/routes/uiRoute.js
@@ -5,8 +5,8 @@ const conn = require('../config/db')
 // CREATE
 router.post('/inventory', (req, res) => {
     const sql = `
-        INSERT INTO inventory (product_id, store_id, stock)
-        VALUES (${req.body.product_id}, ${req.body.store_id}, ${req.body.stock} )
+        INSERT INTO inventory (product_id, store_id, inventory)
+        VALUES (${req.body.product_id}, ${req.body.store_id}, ${req.body.inventory} )
         `
     conn.query(sql, (err, result) => {
         if(err) return res.status(500).send(err)
@@ -18,7 +18,7 @@ router.post('/inventory', (req, res) => {
 // READ
 router.get('/inventory', (req, res) => {
     const sql = `
-        SELECT i.inventory_id  , p.name , s.branch_name , i.stock FROM products p  JOIN inventory i ON p.product_id = i.product_id JOIN stores s ON s.store_id = i.store_id
+        SELECT i.inventory_id  , p.name , s.branch_name , i.inventory FROM products p  JOIN inventory i ON p.product_id = i.product_id JOIN stores s ON s.store_id = i.store_id
     `
 
     conn.query(sql, (err, result) => {
